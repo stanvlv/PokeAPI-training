b2e02e066f3eb9985bbdd54a0ea786c3f5a3fd3a
stanvlv <114359328+stanvlv@users.noreply.github.com>
2022-12-20 13:43:51 +0100

    first commit poke api

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import {Routes, Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route />
+        <Route />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
